13cf52e4936a0ab26e2e4b0965d955b54135a29d
Leonardo Aristizabal <leonardo.aristizabal.m@gmail.com>
2023-03-07 01:47:29 -0400

    Cambio de api por politicas CORS

--- custom.js
+++ custom.js
@@ -1,4 +1,4 @@
-const siteLetters = document.querySelector(".side-right");
+const sideLetters = document.querySelector(".side-right");
 const sideLeft = document.querySelector(".side-left");
 const searchLetters = document.querySelector(".search-letters");
 
@@ -14,26 +14,57 @@ searchLetters.addEventListener('click', (e) => {
         return;
     }
 
-    callApiSong(artista.value, cancion.value);
+    callApiSong(artista, cancion);
 
 })
 
 //Ojo
 
-function callApiSong(artista, cancion){
-    fetch(`https://api.genius.com/search?q=${artista} ${cancion}`, {
-        headers: {
-            Authorization: 'Bearer <-h6BY3hV_FD2Lut22sznlHMSie_fLr6UofzaMWwJq6qoXuBfZqH1AM0R92eBwI4G>'
-        }
+const apiUrl = 'https://api.musixmatch.com/ws/1.1/';
+const apiKey = 'd2420abdcecd2fdc676a4524856b3e34';
+
+function callApiSong() {
+
+  const apiEndpoint = 'track.search';
+  const queryUrl = `${apiUrl}${apiEndpoint}?q_track=${cancion.value}&q_artist=${artista.value}&apikey=${apiKey}`;
+
+
+  fetch(queryUrl)
+    .then(response => response.json())
+    .then(data => {
+//        console.log(data);
+      const trackList = data.message.body.track_list;
+      if (trackList.length > 0) {
+        const trackId = trackList[0].track.track_id;
+        const lyricsEndpoint = 'track.lyrics.get';
+        const lyricsUrl = `${apiUrl}${lyricsEndpoint}?track_id=${trackId}&apikey=${apiKey}`;
+        fetch(lyricsUrl)
+          .then(response => response.json())
+          .then(data => {
+            const lyrics = data.message.body.lyrics.lyrics_body;
+            mostrarLetra(lyrics);
+            console.log(lyrics);
+          })
+          .catch(error => console.error(error));
+      } else {
+        console.log('No se encontraron resultados para la canción');
+      }
     })
-        .then(respuesta => respuesta.json())
-        .then(resultado => {
-            console.log(resultado);
-            // Aquí podrías procesar el resultado y mostrar la letra de la canción
-        })
-        .catch(error => console.log(error));
+    .catch(error => console.error(error));
 }
 
+callApiSong();
+
+function mostrarLetra(lyrics){
+    sideLetters.innerHTML = "";
+    const title = document.createElement("h4");
+    title.innerText = `${cancion.value} by ${artista.value}`; 
+    sideLetters.appendChild(title);
+    const letra = document.createElement("p");
+    letra.innerText = lyrics;
+    letra.style.marginBottom = "20px";
+    sideLetters.appendChild(letra);
+}
 
 
 function mostrarError(mensaje){
